bc48227e0bf994105cabf9c8b5d3505422f3ee73
Trey Morrison <treymorrison94@gmail.com>
2017-12-21 13:02:47 -0500

    Update

---
+++ assets/js/app.js
@@ -0,0 +1,82 @@
+	function buildQuiz() {
+		var output = [];
+
+		myQuestions.forEach((currentQuestion, questionNumber) => {
+			var answers = [];
+
+			for (letter in currentQuestion.answers) {
+				answers.push(
+					`<label>
+						<input type="radio" name="question${questionNumber}" value="${letter}">
+						${letter} :
+						%{currentQuestion.answers[letter]}
+					</label>`
+				);
+			}
+
+			output.push(
+				`<div class="question"> ${currentQuestion.question} </div>
+				<div class="answers"> ${answers.join("")} </div>`
+			);
+		});
+
+		quizContainer.innerHTML = output.join("");
+	}
+
+	function showResults() {
+		var answerContainers = quizContainer.querySelectorAll(".answers");
+		let numCorrect = 0;
+		myQuestions.forEach((currentQuestion, questionNumber) => {
+			var answerContainer = answerContainer[questionNumber];
+			var selector = 'input[name=question${questionNumber}]:checked';
+			var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+			if (userAnswer === currentQuestion.correctAnswer) {
+				numCorrect++;
+				answerContainer[questionNumber].style.color = "lightgreen";
+			} else {
+				answerContainers[questionNumber].styel.color = "red";
+			}
+		});
+
+		resultsContainer.innerHTML = '${numCorrect} out of ${myQuestions.length}';
+	}
+
+	var quizContainer = document.getElementById("quiz");
+	var resultsContainer = document.getElementById("results");
+	var submitButton = document.getElementById("submit");
+	var myQuestions = [
+	{
+		question: "Which is the heaviest Pokemon?",
+		answers: {
+			a: "Groudon",
+			b: "Cosmoem",
+			c: "Celesteela",
+			d: "B and C"
+		},
+		correctAnswer: "d"
+	},
+	{
+		question: "Which is the Pokemon mascot?",
+		answers: {
+			a: "Eevee",
+			b: "Pikachu",
+			c: "Meowth",
+			d: "Jigglypuff"
+		},
+		correctAnswer: "b"
+	},
+	{
+		question: "Which is the first Pokemon?",
+		answers: {
+			a: "Bulbasaur",
+			b: "Arceus",
+			c: "Rhydon",
+			d: "All of the above"
+		},
+		correctAnswer: "d"
+	}	
+	];
+
+	buildQuiz();
+
+	submitButton.addEventListener("click", showResults);
